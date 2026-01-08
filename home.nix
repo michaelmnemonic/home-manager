84b1fef9f0ea24b683e84bad1066c7fcacb90734
michaelmnemonic <michaelmnemonic@posteo.eu>
2026-01-08 15:10:40 +0100

    Deploy niri config

--- home.nix
+++ home.nix
@@ -46,6 +46,7 @@
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
     # ".screenrc".source = dotfiles/screenrc;
+    ".config/niri/config.kdl".source = dotfiles/config.kdl;
 
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
