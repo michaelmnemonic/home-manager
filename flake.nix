573cf19969e0cadd9a79a372177b8bcddd7c8088
michaelmnemonic <michaelmnemonic@posteo.eu>
2026-01-08 14:12:52 +0100

    Add noctalia shell

--- flake.nix
+++ flake.nix
@@ -8,6 +8,10 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    noctalia = {
+      url = "github:noctalia-dev/noctalia-shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
