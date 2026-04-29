6aafd6647f1d00964eb441f81b2afbba7365deee
michaelmnemonic <michaelmnemonic@posteo.eu>
2026-04-29 14:09:43 +0200

    Add walker

--- home.nix
+++ home.nix
@@ -20,9 +20,10 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
-    pkgs.voxtype
+  home.packages = with pkgs; [
+    voxtype
     vibepanel.packages.${pkgs.stdenv.hostPlatform.system}.vibepanel
+    walker
 
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
