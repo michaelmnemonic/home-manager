76e990a044f79db5742f05b8cc69c73c50a6fe4b
michaelmnemonic <michaelmnemonic@posteo.eu>
2026-04-29 13:22:30 +0200

    Replace deprecated concept

--- home.nix
+++ home.nix
@@ -21,7 +21,7 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-    vibepanel.packages.${pkgs.system}.vibepanel
+    vibepanel.packages.${pkgs.stdenv.hostPlatform.system}.vibepanel
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
