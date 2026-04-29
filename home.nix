52814bf321e454c5ddb02054e0e6167d47f6ab23
michaelmnemonic <michaelmnemonic@posteo.eu>
2026-04-29 20:36:45 +0200

    Walker expects elephant as data source

--- home.nix
+++ home.nix
@@ -21,6 +21,7 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
+    elephant
     voxtype
     vibepanel.packages.${pkgs.stdenv.hostPlatform.system}.vibepanel
     walker
