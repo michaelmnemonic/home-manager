c478fb1f54c8d2c8dba464be404527087b6f783a
michaelmnemonic <michaelmnemonic@posteo.eu>
2026-04-29 13:09:08 +0200

    Look-up based on hostname

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "Home Manager configuration of maik";
 
   inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -16,20 +17,21 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-
   outputs = {
     nixpkgs,
     home-manager,
     noctalia,
+    vibepanel,
     ...
-  }: {
-    homeConfigurations."maik" =
+  }: let
+    mkHome = system:
       home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.${builtins.currentSystem};
-        modules = [
-          ./home.nix
-          noctalia.homeModules.default
-        ];
+        pkgs = nixpkgs.legacyPackages.${system};
+        modules = [./home.nix noctalia.homeModules.default];
       };
+  in {
+    homeConfigurations = {
+      "maik@charon" = mkHome "aarch64-linux";
+    };
   };
 }
