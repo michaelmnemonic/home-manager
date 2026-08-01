32a4bf2a573e595e00f6171bfe7cd588044368c2
michaelmnemonic <michaelmnemonic@posteo.eu>
2026-08-01 17:24:04 +0200

    Add dedicated age key

--- hosts/styx.nix
+++ hosts/styx.nix
@@ -2,7 +2,7 @@
   imports = [../modules/common.nix ../modules/syncthing.nix];
 
   config.age = {
-    identityPaths = ["/home/maik/.ssh/id_ed25519"];
+    identityPaths = ["/home/maik/.ssh/id_ed25519" "/home/maik/.config/home-manager/secrets/age.key"];
     secrets = {
       "syncthing_styx_cert.pem".file = ../secrets/syncthing_styx_cert.pem.age;
       "syncthing_styx_key.pem".file = ../secrets/syncthing_styx_key.pem.age;
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,10 +1,11 @@
 let
+  age = "age1um5lyf48x2zryr8c48tt84gw7xamt8c9f7df00jtxtqg0ml2ccts3vr9f9";
   maik_at_styx = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINM1F2ryn8sYFz2SwtPezNMJzFewcZC/WqTY6f6B86vq";
   users = [maik_at_styx];
 
   styx = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICdsANWJFiP9zrry7LG5qws7NTlbMzvDxqfeNTPOZpk1";
   systems = [styx];
 in {
-  "syncthing_styx_cert.pem.age".publicKeys = users ++ systems;
-  "syncthing_styx_key.pem.age".publicKeys = users ++ systems;
+  "syncthing_styx_cert.pem.age".publicKeys = [age] ++ users ++ systems;
+  "syncthing_styx_key.pem.age".publicKeys = [age] ++ users ++ systems;
 }
