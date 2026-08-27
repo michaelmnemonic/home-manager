9761a9a5b07179863a5d228501022e3294bf5b99
michaelmnemonic <michaelmnemonic@posteo.eu>
2026-08-27 13:08:40 +0200

    Import modules

--- hosts/pluto.nix
+++ hosts/pluto.nix
@@ -3,7 +3,13 @@
   t3code,
   ...
 }: {
-  imports = [../modules/common.nix];
+  imports = [
+    ../modules/common.nix
+    ../modules/llama-cpp.nix
+    ../modules/niri.nix
+    ../modules/plasma.nix
+    ../modules/syncthing.nix
+  ];
 
   home.packages = [
     t3code.packages.${pkgs.stdenv.hostPlatform.system}.t3code-opencode
