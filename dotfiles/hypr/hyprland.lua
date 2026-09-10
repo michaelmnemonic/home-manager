4d5e6c0b6b34630c0ff5e724522f1c28ca047994
michaelmnemonic <michaelmnemonic@posteo.eu>
2026-09-10 23:40:23 +0200

    Use purple border

--- dotfiles/hypr/hyprland.lua
+++ dotfiles/hypr/hyprland.lua
@@ -111,8 +111,8 @@ hl.config({
         border_size = 2,
 
         col = {
-            active_border   = { colors = {"rgba(33ccffee)", "rgba(00ff99ee)"}, angle = 45 },
-            inactive_border = "rgba(595959aa)",
+            active_border   = "rgba(DC8ADDAA)",
+            inactive_border = "rgba(505050AA)",
         },
 
         -- Set to true to enable resizing windows by clicking and dragging on borders and gaps
