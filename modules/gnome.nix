aa286a07a4c94f384b849546c3bb9aa2049458d4
michaelmnemonic <michaelmnemonic@posteo.eu>
2026-09-13 17:12:31 +0200

    Combine dconf settings

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -10,28 +10,29 @@ in {
   };
   imports = [./voxtype.nix];
   config = lib.mkIf cfg.enable {
-    dconf.enable = true;
-
-    dconf.settings = {
-      "org/gnome/desktop/interface" = {
-        accent-color = "purple";
-      };
-      "org/gnome/desktop/wm/preferences" = {
-        button-layout = ":close";
-      };
-      "org/gnome/shell" = {
-        favorite-apps = [
-          "org.gnome.Nautilus.desktop"
-          "firefox.desktop"
-          "org.gnome.Calendar.desktop"
-          "io.github.quodlibet.QuodLibet.desktop"
-          "kodi.desktop"
-          "steam.desktop"
-          "dev.zed.Zed.desktop"
-          "code.desktop"
-          "t3code.desktop"
-          "org.gnome.Console.desktop"
-        ];
+    dconf = {
+      enable = true;
+      settings = {
+        "org/gnome/desktop/interface" = {
+          accent-color = "purple";
+        };
+        "org/gnome/desktop/wm/preferences" = {
+          button-layout = ":close";
+        };
+        "org/gnome/shell" = {
+          favorite-apps = [
+            "org.gnome.Nautilus.desktop"
+            "firefox.desktop"
+            "org.gnome.Calendar.desktop"
+            "io.github.quodlibet.QuodLibet.desktop"
+            "kodi.desktop"
+            "steam.desktop"
+            "dev.zed.Zed.desktop"
+            "code.desktop"
+            "t3code.desktop"
+            "org.gnome.Console.desktop"
+          ];
+        };
       };
     };
 
